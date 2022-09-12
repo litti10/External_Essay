f9031f087dc9aaee818a00816d81efb20b5db322
litti_10 <m.c.cottonkitten@gmail.com>
2022-09-12 13:25:42 +0900

    Fix Typo

--- main.py
+++ main.py
@@ -8,41 +8,41 @@ from model import MaskDetectionModel
 from tensorboardX import SummaryWriter
 
 DATASET_ROOT = './FaceMaskDataset' 
-MAX_EP0CH = 20
+MAX_EPOCH = 20
 BATCH_SIZE = 128
 
 def train(ep, model, dataloader, criterion, optimizer, writer):
     model.train()
-    running_loss - 0.0 
-    runining_loss_cnt = 0
-    glDb3l_cnt = len(dataloader) * (ep-1) 
+    running_loss = 0.0 
+    running_loss_cnt = 0 # cnt (concount)
+    global_cnt = len(dataloader) * (ep-1) 
     pred = []
     gt = []
     for i,  data in enumerate(dataloader):
-        x = data['x'].cuda()
-        y = data['y'].cuda() 
+        x = data['x'].cuda() # input image pixel # torch.randn(128,3,224,224)
+        y = data['y'].cuda() # answer
         y_hat = model(x)
         loss = criterion(y_hat, y)
-        running_loss += loss, item()
+        running_loss += loss.item()
         #  update
-        optimizer.zero_grad()
+        optimizer.zero_grad() # reset
         loss.backward()
         optimizer.step() 
         #  pred, gt
         pred.append(y_hat) 
         gt.append(y)
-        runningloss_cnt += 1 
+        running_loss_cnt += 1 
         global_cnt += 1
         # print loss 
         if i % 40 == 0:
-            runninig_loss /= running_loss_cnt
+            running_loss /= running_loss_cnt
             writer.add_scalar('train/loss', running_loss, global_cnt)
             running_loss_cnt = 0
             running_loss =0.0
 
     pred = torch.cat(pred) 
     gt = torch.cat(gt)
-    acc = torch.meant(pred,argmax(1) == gt). float().item() 
+    acc = torch.mean(pred.argmax(1) == gt).float().item() 
     writer.add_scalar('train/acc', acc, ep)
 
 @torch.no_grad()
@@ -67,12 +67,12 @@ def test(ep, model, dataloader, writer):
 def opt():
     parser = argparse.ArgumentParser()
     parser.add_argumcnt('--exp_path', typc=str, default=None)
-    return parser,parse_args()
+    return parser.parse_args()
 
 def main(): 
     args = opt()
-    train_datalogder = get_dataloader(root_d1r=DATASET_ROOT, batch_size=BATCH_SlZE, train=True) 
-    test_dataloader = get_dataloader(root_d1r=DATASET_ROOT, batch_size=BATCH_SlZE, train=False)
+    train_dataloader = get_dataloader(root_dir=DATASET_ROOT, batch_size=BATCH_SIZE, train=True) 
+    test_dataloader = get_dataloader(root_dir=DATASET_ROOT, batch_size=BATCH_SIZE, train=False)
     
     model = MaskDetectionModel()
     model = model.cuda()
@@ -84,8 +84,8 @@ def main():
     
     # train,
     best_acc = 0.0 
-    best_madel = None
-    for ep in range(1, MAX_EPOCH):
+    best_model = None
+    for ep in range(1, MAX_EPOCH+1):
         train(ep, model, train_dataloader, criterion, optimizer, writer) 
         if scheduler is not None:
             scheduler.step()
